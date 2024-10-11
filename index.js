0a95af6b48b12478d24845ffc66f593506c973c1
Min <mail4akkm@gmail.com>
2024-10-11 19:37:39 +0000

    adding routes

--- index.js
+++ index.js
@@ -25,8 +25,17 @@ app.use(
     })
 );
 
+// import in routes 
+const landingRoutes = require('./routes/landing'); 
+
 async function main() {
-}
+
+   app.use('/',landingRoutes);
+
+} 
+
+
+
 main();
 app.listen(3000, () => {
     console.log("Server has started");
